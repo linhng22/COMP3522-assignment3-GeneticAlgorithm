444cdb70ee28208e0a93a4573f63e562cf97fa16
ngocanhnt269 <ngocanhnt269@gmail.com>
2023-11-23 11:42:02 -0800

    define the singleton for Genetic Algorithms and add the variable

--- GeneticAlgorithms.cpp
+++ GeneticAlgorithms.cpp
@@ -1,1 +1,3 @@
 #include "GeneticAlgorithms.hpp"
+
+
--- GeneticAlgorithms.hpp
+++ GeneticAlgorithms.hpp
@@ -1,8 +1,26 @@
 #ifndef LAB1TEMPLATE_GENETICALGORITHMS_HPP
 #define LAB1TEMPLATE_GENETICALGORITHMS_HPP
 
+#include "City.hpp"
+#include <vector>
+#include <random>
+
+#define CITIES_IN_TOUR 32
+#define POPULATION_SIZE 32
+#define ITERATIONS 1000
 
 class GeneticAlgorithms {
+private:
+    int CitiesInTour;
+    int PopulationSize;
+    GeneticAlgorithms() = default;
+public:
+    static GeneticAlgorithms &getInstance() {
+        static GeneticAlgorithms instance;
+        return instance;
+    }
+    GeneticAlgorithms(GeneticAlgorithms &instance) = delete;
+    void operator=(GeneticAlgorithms &instance) = delete;
 
 };
 
