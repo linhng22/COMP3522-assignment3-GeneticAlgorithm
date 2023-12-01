a4245bb0e992f774662cc2bea1f543b9fd0989fb
ngocanhnt269 <ngocanhnt269@gmail.com>
2023-11-30 16:17:03 -0800

    Added comments for Tour, city and SingleTon

--- SingletonFacade.hpp
+++ SingletonFacade.hpp
@@ -8,15 +8,24 @@
 
 class SingletonFacade {
 private:
+    // Default constructor is private to prevent external instantiation.
     SingletonFacade()=default;
 public:
+
+    // Method to retrieve the instance of SingletonFacade
     static SingletonFacade &getInstance() {
+        // Static instance creation ensures only one instance is created during the program's lifetime.
         static SingletonFacade singleton_facade;
+        // Returns the reference to the SingletonFacade instance.
         return singleton_facade;
     }
+    // Deleted copy constructor to prevent copying of SingletonFacade instance.
     SingletonFacade(const SingletonFacade &) = delete;
+
+    // Deleted copy assignment operator to prevent assignment of SingletonFacade instance.
     SingletonFacade &operator=(const SingletonFacade &) = delete;
 
+    // Default destructor.
     ~SingletonFacade() = default;
 };
 
--- Tour.cpp
+++ Tour.cpp
@@ -31,13 +31,17 @@ double Tour::getDistanceRating() const {
 
 void Tour::computeDistance() {
     double distance = 0;
+    // Loop through the cities to calculate the total distance of the tour
     for(size_t i =0; i < this->getCitiesToVisit().size() - 1; ++i){
+        // Accumulate the distances between consecutive cities
         distance += getDistanceBetweenCity(this->CitiesToVisit[i], this->CitiesToVisit[i+1]);
     }
+    // Set the total distance as the FitnessRating of the tour
     this->FitnessRating = distance;
 }
 
 bool operator<(const Tour &lhs, const Tour &rhs) {
+    // Check id the distance from the lhs is smaller than rhs
     if(lhs.getDistanceRating() < rhs.getDistanceRating()){
         return true;
     }
@@ -54,22 +58,31 @@ ostream &operator<<(ostream &os, const Tour &tour) {
     }
     return os;
 }
+
 bool operator==(const Tour &lhs, const Tour &rhs) {
     return lhs.FitnessRating == rhs.FitnessRating;
 }
 
 bool Tour::containsCity(const City *otherCity) const {
-    bool isContained = any_of(CitiesToVisit.begin(), CitiesToVisit.end(), [&otherCity](const City *pCity) {
+    // Initialize a boolean variable to store if the city is contained in the tour
+    bool isContained = any_of(CitiesToVisit.begin(),
+                              CitiesToVisit.end(), [&otherCity](const City *pCity) {
+        // Lambda function checks if any city in the tour matches the 'otherCity'
         return *pCity == *otherCity;
     });
+    // Return the result whether the 'otherCity' is contained in the tour
     return isContained;
 }
 
 void Tour::swapCity(int first, int second) {
+    // Get an iterator pointing to the beginning of the list of cities
     auto it = CitiesToVisit.begin();
+
+    // Check if the 'first' city is the last city in the tour, then set 'second' to 0
     if ((int) CitiesToVisit.size() == first + 1) {
         second = 0;
     }
+    // Swap the cities at positions 'first' and 'second'
     iter_swap(it + first, it + second);
 }
 
